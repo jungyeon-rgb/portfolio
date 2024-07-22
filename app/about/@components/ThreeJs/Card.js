c4e720825d56551992d63c6831a58de2a660bd06
김정연 <jy.keem2468@gmail.com>
2024-07-22 16:53:59 +0900

    카드 크기 설정

---
+++ app/about/@components/ThreeJs/Card.js
@@ -0,0 +1,16 @@
+import * as THREE from "three";
+
+// const card = new Card(); // 카드 값을 설정하는 곳
+
+class Card {
+  constructor({ width, height, color }) {
+    const geometry = new THREE.PlaneGeometry(width, height);
+    const material = new THREE.MeshStandardMaterial({ color });
+
+    const mesh = new THREE.Mesh(geometry, material);
+
+    this.mesh = mesh;
+  }
+}
+
+export default Card;
--- app/about/@components/ThreeJs/main.js
+++ app/about/@components/ThreeJs/main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import Card from "./Card";
 
 window.addEventListener("load", function () {
   init();
@@ -19,5 +20,34 @@ function init() {
 
   scene.background = texture;
 
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    500
+  );
+
+  camera.position.z = 25;
+
+  const card = new Card({
+    width: 10,
+    height: 15.8,
+    color: "#0077ff",
+  });
+
+  scene.add(card.mesh);
+
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+
+  ambientLight.position.set(-5, -5, -5);
+
+  scene.add(ambientLight);
+
   render();
+
+  function render() {
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
 }
