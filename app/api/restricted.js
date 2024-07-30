8dbb1c18abbcb35cb0131c05c02023184f846064
jungyeon-rgb <jy.keem2468@gmail.com>
2024-07-30 12:12:41 +0900

    build error 해결

--- app/api/restricted.js
+++ app/api/restricted.js
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "./auth/[...nextauth]"
-export default async (req, res) => {
+;async (req, res) => {
     const session = await getServerSession(req, res, authOptions)
     if (session) {
         res.send({
