bb274d4210d6eab489f42c5d3c3102a0dfc9a74b
jungyeon-rgb <jy.keem2468@gmail.com>
2024-07-30 11:04:28 +0900

    guestbook input form 디자인 수정

--- app/guestbook/components/AuthButtons.jsx
+++ app/guestbook/components/AuthButtons.jsx
@@ -1,38 +1,33 @@
-import { signIn, useSession } from "next-auth/react";
-import Image from "next/image";
+import { signIn, useSession } from "next-auth/react"
+import Image from "next/image"
 
 const AuthButtons = ({ handleCommentSubmit }) => {
-  const { data: session } = useSession();
+    const { data: session } = useSession()
 
-  return (
-    <div className="mb-4">
-      {session ? (
-        <>
-          <button
-            onClick={handleCommentSubmit}
-            className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600"
-          >
-            Comment
-          </button>
-        </>
-      ) : (
-        <>
-          <button
-            onClick={() => signIn("github")}
-            className="flex justify-center items-center px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600"
-          >
-            <Image
-              src="/images/github.svg"
-              width={40}
-              height={40}
-              alt="githubicon"
-            />
-            Sign in with GitHub
-          </button>
-        </>
-      )}
-    </div>
-  );
-};
+    return (
+        <div className="mb-4">
+            {session ? (
+                <>
+                    <button
+                        onClick={handleCommentSubmit}
+                        className="w-[150px] px-4 py-2 leading-5 text-white bg-green-500 rounded-md hover:bg-green-600"
+                    >
+                        Comment
+                    </button>
+                </>
+            ) : (
+                <>
+                    <button
+                        onClick={() => signIn("github")}
+                        className="w-[150px] px-4 py-2 leading-5 text-white bg-green-500 rounded-md hover:bg-green-600"
+                    >
+                        <Image src="/images/github.svg" width={40} height={40} alt="githubicon" />
+                        Sign in with GitHub
+                    </button>
+                </>
+            )}
+        </div>
+    )
+}
 
-export default AuthButtons;
+export default AuthButtons
--- app/guestbook/page.jsx
+++ app/guestbook/page.jsx
@@ -1,171 +1,163 @@
-"use client";
-import { useSession } from "next-auth/react";
-import AuthButtons from "./components/AuthButtons";
-import { useState, useEffect } from "react";
-import Image from "next/image";
-import { marked } from "marked"; // marked 라이브러리 임포트
-import matter from "gray-matter"; // gray-matter 임포트
-import fs from "fs"; // 파일 시스템 모듈
-import path from "path"; // 경로 모듈
-import { serialize } from "next-mdx-remote"; // MDX 직렬화
+"use client"
+import { useSession } from "next-auth/react"
+import AuthButtons from "./components/AuthButtons"
+import { useState, useEffect } from "react"
+import Image from "next/image"
+import { marked } from "marked" // marked 라이브러리 임포트
+import matter from "gray-matter" // gray-matter 임포트
+import path from "path" // 경로 모듈
+import { serialize } from "next-mdx-remote" // MDX 직렬화
 
 const GuestBook = () => {
-  const { data: session } = useSession();
-  const [comment, setComment] = useState("");
-  const [commentsList, setCommentsList] = useState([]);
-  const [activeTab, setActiveTab] = useState("write");
-  const [markdownContent, setMarkdownContent] = useState(""); // Markdown 내용 상태
+    const { data: session } = useSession()
+    const [comment, setComment] = useState("")
+    const [commentsList, setCommentsList] = useState([])
+    const [activeTab, setActiveTab] = useState("write")
+    const [markdownContent, setMarkdownContent] = useState("") // Markdown 내용 상태
 
-  useEffect(() => {
-    // Markdown 파일 로드
-    const loadMarkdown = async () => {
-      const filePath = path.join(process.cwd(), "content/sample.md");
-      const fileContent = fs.readFileSync(filePath, "utf8");
-      const { content } = matter(fileContent);
-      const serializedContent = await serialize(content);
-      setMarkdownContent(serializedContent);
-    };
-    loadMarkdown();
-  }, []);
+    // useEffect(() => {
+    //     // Markdown 파일 로드
+    //     const loadMarkdown = async () => {
+    //         const filePath = path.join(process.cwd(), "content/sample.md")
+    //         const fileContent = fs.readFileSync(filePath, "utf8")
+    //         const { content } = matter(fileContent)
+    //         const serializedContent = await serialize(content)
+    //         setMarkdownContent(serializedContent)
+    //     }
+    //     loadMarkdown()
+    // }, [])
 
-  const handleCommentChange = (e) => {
-    setComment(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (comment.trim()) {
-      setCommentsList([
-        ...commentsList,
-        {
-          text: comment,
-          user: session?.user || {
-            email: "Anonymous",
-            image: "/path/to/default/image.png",
-          },
-        },
-      ]);
-      setComment("");
-      setActiveTab("write");
+    const handleCommentChange = (e) => {
+        setComment(e.target.value)
     }
-  };
 
-  const handleCommentSubmit = () => {
-    handleSubmit(new Event("submit", { cancelable: true }));
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (comment.trim()) {
+            setCommentsList([
+                ...commentsList,
+                {
+                    text: comment,
+                    user: session?.user || {
+                        email: "Anonymous",
+                        image: "/path/to/default/image.png",
+                    },
+                },
+            ])
+            setComment("")
+            setActiveTab("write")
+        }
+    }
 
-  return (
-    <section className="w-screen h-screen flex flex-col justify-center items-center text-black">
-      <video
-        className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
-        src="/video/background1.mp4"
-        loop
-        autoPlay
-        muted
-      />
-      <h1 className="text-9xl text-white font-stardom mb-4">GUEST BOOK</h1>
+    const handleCommentSubmit = () => {
+        handleSubmit(new Event("submit", { cancelable: true }))
+    }
 
-      <div className="w-3/4 max-w-2xl mx-auto p-6 bg-gray-100 rounded-lg shadow-md">
-        {commentsList.length > 0 && (
-          <div className="mt-6">
-            <h3 className="text-lg font-bold mb-2">Comments</h3>
-            <div className="bg-white border border-gray-300 rounded-md p-4">
-              {commentsList.map((cmt, index) => (
-                <div
-                  key={index}
-                  className="mb-2 p-2 border-b border-gray-200 flex items-start"
-                >
-                  <Image
-                    src={cmt.user.image}
-                    alt="Profile"
-                    width={100}
-                    height={100}
-                    className="w-8 h-8 rounded-xl mr-2"
-                  />
-                  <div>
-                    <p className="font-bold">{cmt.user.email}</p>
-                    <div
-                      className="text-black"
-                      dangerouslySetInnerHTML={{ __html: marked(cmt.text) }}
-                    />
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        )}
-        <div className="flex flex-col">
-          {session && (
-            <Image
-              src={session.user.image}
-              alt="Profile"
-              width={40}
-              height={40}
-              className="w-10 h-10 rounded-xl mr-2"
+    return (
+        <section className="flex flex-col items-center justify-center w-screen h-full text-black">
+            <video
+                className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
+                src="/video/background1.mp4"
+                loop
+                autoPlay
+                muted
             />
-          )}
-          <div>
-            <h2 className="text-lg font-xl mb-4">Write</h2>
-            <div className="flex mb-4">
-              <button
-                className={`px-4 py-2 ${
-                  activeTab === "write"
-                    ? "bg-green-500 text-white"
-                    : "bg-gray-200"
-                }`}
-                onClick={() => setActiveTab("write")}
-              >
-                Write
-              </button>
-              <button
-                className={`px-4 py-2 ${
-                  activeTab === "preview"
-                    ? "bg-green-500 text-white"
-                    : "bg-gray-200"
-                }`}
-                onClick={() => setActiveTab("preview")}
-              >
-                Preview
-              </button>
-            </div>
-            {activeTab === "write" && (
-              <form onSubmit={handleSubmit} className="flex flex-col">
-                <textarea
-                  className="w-full h-32 p-3 border border-gray-300 rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-green-400"
-                  placeholder="Leave a comment"
-                  value={comment}
-                  onChange={handleCommentChange}
-                />
-                <div className="flex justify-between items-center my-4">
-                  <span className="text-sm text-gray-500">
-                    Styling with Markdown is supported
-                  </span>
-                  <AuthButtons handleCommentSubmit={handleCommentSubmit} />
-                </div>
-              </form>
-            )}
-            {activeTab === "preview" && (
-              <div className="mt-4 p-4 border border-gray-300 rounded-md">
-                <h3 className="text-lg font-bold mb-2">Preview</h3>
-                <div
-                  className="text-black"
-                  dangerouslySetInnerHTML={{ __html: marked(comment) }} // 미리보기
-                />
-              </div>
-            )}
-            {/* Markdown 콘텐츠 렌더링 */}
-            <div className="mt-4 p-4 border border-gray-300 rounded-md">
-              <h3 className="text-lg font-bold mb-2">Markdown Content</h3>
-              <div
-                className="text-black"
-                dangerouslySetInnerHTML={{ __html: marked(markdownContent) }} // Markdown 콘텐츠
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
+            <h1 className="my-4 text-white text-9xl font-stardom">GUEST BOOK</h1>
 
-export default GuestBook;
+            <div className="w-3/4 max-w-2xl p-6 mx-auto rounded-lg shadow-lg">
+                {/* 댓글 작성된 목록 확인 */}
+                {commentsList.length > 0 && (
+                    <div className="mt-6">
+                        <h3 className="mb-2 text-lg font-bold">Comments</h3>
+                        <div className="p-4 bg-white border border-gray-300 rounded-md">
+                            {commentsList.map((cmt, index) => (
+                                <div
+                                    key={index}
+                                    className="flex bg-[#e1e4e8] items-start p-2 mb-2 border-b border-gray-200"
+                                >
+                                    <div>
+                                        <p className="font-bold solid ">{cmt.user.name}</p>
+                                        <div
+                                            className="text-black"
+                                            dangerouslySetInnerHTML={{ __html: marked(cmt.text) }}
+                                        />
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                )}
+                {/* 댓글 달 때 */}
+                <div className="flex my-6">
+                    <div className="flex justify-center">
+                        {session && (
+                            <Image
+                                src={session.user.image}
+                                alt="Profile"
+                                width={40}
+                                height={40}
+                                className="w-10 h-10 mr-2 rounded-xl"
+                            />
+                        )}
+                    </div>
+                    <div className="flex-row items-center justify-center w-full ml-4">
+                        <div className="flex pt-2 px-2 bg-[#e1e4e8] rounded-t-md">
+                            <button
+                                className={`px-4 py-2 rounded-t-md border-t-2 ${
+                                    activeTab === "write" ? "bg-white" : "bg-gray-200"
+                                }`}
+                                onClick={() => setActiveTab("write")}
+                            >
+                                Write
+                            </button>
+                            <button
+                                className={`px-4 py-2 rounded-t-md border-t-2 ${
+                                    activeTab === "preview" ? "bg-white" : "bg-gray-200"
+                                }`}
+                                onClick={() => setActiveTab("preview")}
+                            >
+                                Preview
+                            </button>
+                        </div>
+                        {activeTab === "write" && (
+                            <form onSubmit={handleSubmit} className="flex flex-col">
+                                <textarea
+                                    className="w-full h-32 p-3 rounded-l resize-none focus:rounded-md focus:outline-none focus:ring-2 rounded-b-md focus:ring-blue-400"
+                                    placeholder="Leave a comment"
+                                    value={comment}
+                                    onChange={handleCommentChange}
+                                />
+                                <div className="flex items-center justify-between my-4">
+                                    <span className="text-sm text-gray-500">Styling with Markdown is supported</span>
+                                    <AuthButtons handleCommentSubmit={handleCommentSubmit} />
+                                </div>
+                            </form>
+                        )}
+                        {activeTab === "preview" && (
+                            <form onSubmit={handleSubmit} className="flex flex-col">
+                                <div
+                                    className="w-full h-32 p-3 bg-white rounded-l resize-none focus:rounded-md focus:outline-none focus:ring-2 rounded-b-md focus:ring-blue-400"
+                                    dangerouslySetInnerHTML={{ __html: marked(comment) }}
+                                />
+                                <div className="flex items-center justify-between my-4">
+                                    <span className="text-sm text-gray-500">Styling with Markdown is supported</span>
+                                    <AuthButtons handleCommentSubmit={handleCommentSubmit} />
+                                </div>
+                            </form>
+                        )}
+                        {/* Markdown 콘텐츠 렌더링
+                        <div className="p-4 mt-4 border border-gray-300 rounded-md">
+                            <h3 className="mb-2 text-lg font-bold">Markdown Content</h3>
+                            <div
+                                className="text-black"
+                                dangerouslySetInnerHTML={{ __html: marked(markdownContent) }} // Markdown 콘텐츠
+                            />
+                        </div> */}
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default GuestBook
