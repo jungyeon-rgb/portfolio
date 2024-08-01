cf25ad571eb3d2a49238e4075505f65efc247d82
hayoonPark <the.day0619@gmail.com>
2024-08-01 14:23:00 +0900

    update 방명록 엔티티 생성

---
+++ back/portfolio/src/main/java/jungyeon/portfolio/domain/comment/entity/Comment.java
@@ -0,0 +1,38 @@
+package jungyeon.portfolio.domain.comment.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jungyeon.portfolio.global.entity.AuditBaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@ToString
+@Table(name = "comment")
+public class Comment extends AuditBaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "comment_id", nullable = false)
+	private Integer id;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "comment")
+	private String comment;
+}
