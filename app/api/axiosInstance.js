bc07699a313731417073e8725ef4010ccf3d3506
김정연 <jy.keem2468@gmail.com>
2024-08-09 17:40:11 +0900

    cors에러 해결을 위한 axiosInstance 코드 추가

--- app/api/axiosInstance.js
+++ app/api/axiosInstance.js
@@ -1,10 +1,14 @@
-import axios from "axios"
+import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://43.200.112.139:8080/api",
-    headers: {
-        "Content-Type": "application/json",
-    },
-})
+  baseURL: "http://43.200.112.139:8080/api",
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*", // 모든 도메인 허용
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS", // 허용할 HTTP 메서드
+    "Access-Control-Allow-Headers": "Content-Type, Authorization", // 허용할 HTTP 헤더
+  },
+  withCredentials: true, // 쿠키나 인증 정보를 포함하기 위해
+});
 
-export default axiosInstance
+export default axiosInstance;
