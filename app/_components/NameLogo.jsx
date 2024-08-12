64332b1e2e8cd7b43f264b9ca62c741f0e866f14
김정연 <jy.keem2468@gmail.com>
2024-08-13 08:15:43 +0900

    namelogo text 변경

--- app/_components/NameLogo.jsx
+++ app/_components/NameLogo.jsx
@@ -1,32 +1,35 @@
-import Image from "next/image"
+import Image from "next/image";
 
 // TODO : https://gsap.com/community/forums/topic/39392-animated-text-circle-with-next-js/
 
 const NameLogo = () => {
-    const text = "For a moment of rest between lives stay."
+  //   const text = "For a moment of rest between lives stay.";
+  const text = "♪";
 
-    return (
-        <section className="relative w-fit h-fit flex items-center justify-center">
-            <div className="relative flex items-center justify-center">
-                <Image src="/images/cloud.svg" width={150} height={150} alt="cloud" />
-                <div className="circle-text absolute w-full h-full flex items-center justify-center text-4xl">
-                    {Array.from(text).map((char, index) => (
-                        <span
-                            key={index}
-                            className="absolute text-white"
-                            style={{
-                                transform: `rotate(${index * (360 / text.length)}deg) translate(100px) rotate(-${
-                                    index * (360 / text.length)
-                                }deg)`,
-                            }}
-                        >
-                            {char}
-                        </span>
-                    ))}
-                </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className="relative w-fit h-fit flex items-center justify-center">
+      <div className="relative flex items-center justify-center">
+        <Image src="/images/cloud.svg" width={150} height={150} alt="cloud" />
+        <div className="circle-text absolute w-full h-full flex items-center justify-center text-4xl">
+          {Array.from(text).map((char, index) => (
+            <span
+              key={index}
+              className="absolute text-white"
+              style={{
+                transform: `rotate(${
+                  index * (360 / text.length)
+                }deg) translate(100px) rotate(-${
+                  index * (360 / text.length)
+                }deg)`,
+              }}
+            >
+              {char}
+            </span>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default NameLogo
+export default NameLogo;
